a14cbc3d14467c2e02fe3bfed7dca19ce8610853
Rodrigo Viana <digo1209@gmail.com>
2024-03-12 12:07:52 -0300

    components dinamicos criados

---
+++ src/app/data/ddaataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+    {
+        "id":1,
+        "photo":"https://th.bing.com/th/id/OIP.u3mgmYMq4ofTd4lBtZtbxQHaD4?rs=1&pid=ImgDetMain",
+        "title":"Angular: Nova Versão Disponível",
+        "description":"Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem "
+    },
+    {
+        "id":5,
+        "photo":"https://th.bing.com/th/id/OIP.33CwBYkmnMfpA9Djup22JwHaHa?rs=1&pid=ImgDetMain",
+        "title":"React: Nova Versão Disponível",
+        "description":"Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem "
+    }
+
+]
